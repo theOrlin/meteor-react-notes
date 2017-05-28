4f4291523c15c5bfb624bac598e29ca83129453e
Orlin Ivanov <o_dogg@hotmail.com>
2017-05-28 21:37:42 +0300

    Mark selected note in list

--- imports/routes/routes.js
+++ imports/routes/routes.js
@@ -6,6 +6,7 @@ import Signup from '../ui/Signup';
 import Dashboard from '../ui/Dashboard';
 import NotFound from '../ui/NotFound';
 import Login from '../ui/Login';
+import { Session } from 'meteor/session';
 
 const unauthenticatedPages = ['/', '/signup'];
 const authenticatedPages = ['/dashboard'];
@@ -14,11 +15,21 @@ const onEnterPublicPage = () => {
     browserHistory.replace('/dashboard');
   }
 };
+
 const onEnterPrivatePage = () => {
   if (!Meteor.userId()) {
     browserHistory.replace('/');
   }
 };
+
+const onEnterNotePage = (nextState) => {
+  if (!Meteor.userId()) {
+    browserHistory.replace('/');
+  } else {
+    Session.set('selectedNoteId', nextState.params.id);
+  }
+};
+
 export const onAuthChange = (isAuthenticated) => {
   const pathname = browserHistory.getCurrentLocation().pathname;
   const isUnauthenticatedPage = unauthenticatedPages.includes(pathname);
@@ -30,12 +41,13 @@ export const onAuthChange = (isAuthenticated) => {
     browserHistory.replace('/');
   }
 };
+
 export const routes = (
   <Router history={browserHistory}>
     <Route path="/" component={Login} onEnter={onEnterPublicPage} />
     <Route path="/signup" component={Signup} onEnter={onEnterPublicPage} />
     <Route path="/dashboard" component={Dashboard} onEnter={onEnterPrivatePage} />
-    <Route path="/dashboard/:id" component={Dashboard} onEnter={onEnterPrivatePage} />
+    <Route path="/dashboard/:id" component={Dashboard} onEnter={onEnterNotePage} />
     <Route path="*" component={NotFound} />
   </Router>
 );
